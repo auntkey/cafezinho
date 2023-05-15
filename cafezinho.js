c7b274c0bad501c2d4c5a0c064539aa5bd6ddd2b
Keylla <92216717+auntkey@users.noreply.github.com>
2023-05-15 15:46:47 -0300

    Update cafezinho.js

--- cafezinho.js
+++ cafezinho.js
@@ -2,5 +2,5 @@ const BotaoMudarTema = document.getElementById("botaozinho");
 const body = document.querySelector("body");
 
 BotaoMudarTema.addEventListener("click", () =>{
-body.classlist.add("modoescuro");})
+body.classList.add("modoescuro");})
 }
